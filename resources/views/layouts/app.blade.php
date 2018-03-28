1575a3f9bb8f6406b7bc0644800c428b750149ad
Candra Herdiansyah <candraherdiansyah14@gmail.com>
2018-03-28 09:14:07 +0700

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        Relasi Data
+                        Relasi
                     </a>
                 </div>
 
